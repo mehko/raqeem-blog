2327dcd8fab4c1c19a5df93cda479e75a4a93b6e
mehko <mbadrani87@gmail.com>
2022-07-29 16:41:49 +0200

    changing footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+      All right reserved. Made with{" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by <Link to="/">raqeem-blog.netlify.app</Link>
       </p>
     </div>
   </footer>
